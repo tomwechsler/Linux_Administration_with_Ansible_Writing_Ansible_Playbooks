8ef4719b26f701c10a30f4771568845698f5f786
Tom Wechsler <tom.wechsler@outlook.com>
2022-12-03 16:14:23 +0100

    lines edited and new file

--- A02/04_Syntax_playbook_check.sh
+++ A02/04_Syntax_playbook_check.sh
@@ -9,28 +9,23 @@ vim tree.yaml
 - name: Install a package
   hosts: all
   become: true
-  gather_facts: true
   tasks:
     - name: Install tree
       package:
         name: tree
         state: present
-    - name: Print Progress
-      debug:
-        msg: "This is {{ ansible_distribution }}"
-
 
 #Exit the editor
 
-#Install the ansible-lint feature
+#Install the yamllint feature
 sudo yum install yamllint
 
 #We can install the ansible-lint Python modules that will check you Playbook against style guidelines.
-ansible-lint -v FirstPlay.yaml
+yamllint tree.yaml
 
 #We can also check the correct syntax is employed in the playbook.
-ansible-playbook FirstPay.yaml --syntax-check
+ansible-playbook tree.yaml --syntax-check
 
 #Going beyond syntax checking we can try the option -C to check the operation without
 #implementing change. This works best with the verbose option -v.
-ansible-playbook -C -v FirstPlay.yaml
+ansible-playbook -C -v tree.yaml
---
+++ A02/05_Run_playbook_and_extend.sh
@@ -0,0 +1,21 @@
+#Now we have worked with the first variables. Now imagine if we can create the variables based on facts. 
+#The following example:
+
+ansible centos -m setup
+
+ansible centos -m setup -a "filter=*family*"
+
+vim tree.yaml
+
+- name: Install a package
+  hosts: all
+  become: true
+  gather_facts: true
+  tasks:
+    - name: Install tree
+      package:
+        name: tree
+        state: present
+    - name: Print Progress
+      debug:
+        msg: "This is {{ ansible_distribution }}"

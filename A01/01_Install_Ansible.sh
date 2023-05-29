1e44849700cd5634da376a5faaee4074554f37e7
Tom Wechsler <tom.wechsler@outlook.com>
2023-05-29 15:11:11 +0200

    lines edited

--- A01/01_Install_Ansible.sh
+++ A01/01_Install_Ansible.sh
@@ -1,20 +1,23 @@
 #If yout get some locale warnings
 export LC_ALL="en_US.UTF-8"
 
+#If this does not work, use:
+sudo dnf install -y glibc-langpack-en
+
 #Install the epel release
-sudo yum install -y epel-release
+sudo dnf install -y epel-release
 
 #Install python3
-sudo yum install -y python3
+sudo dnf install -y python3
 
 #If more then one python version is on the host
 sudo alternatives --set python /usr/bin/python3
 
 #Install Ansible
-sudo yum install -y ansible
+sudo dnf install -y ansible
 
 #Install python arg-completion
-sudo yum install -y python3-argcomplete
+sudo dnf install -y python3-argcomplete
 
 #Set argcomplete
 sudo activate-global-python-argcomplete
